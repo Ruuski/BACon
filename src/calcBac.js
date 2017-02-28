0ea4a0414441f231255d83167c1661a0a6aa6d72
Liam <liam.aharon@hotmail.com>
2017-02-28 23:41:39 +1100

    removed redundant code due to bug fix in main

--- src/calcBac.js
+++ src/calcBac.js
@@ -1,12 +1,10 @@
 const KG_TO_GRMS = 1000;
 const MS_TO_HR = 1/3600000;
-const SAFETY_CONST = 1.02;  // give buffer of 2% to BAC to account for errors
 
-calcBac = (gramsAlc, bodyWeightKg, genderConstant, timeIngested) => {
+calcBac = (gramsAlc, bodyWeightKg, genderConstant) => {
   curTime = new Date().getTime();
-  hrsElapsed = (curTime - timeIngested) * MS_TO_HR;
   bodyWeightGrams = bodyWeightKg * KG_TO_GRMS;
-  return ((gramsAlc / (bodyWeightGrams * genderConstant)) * 100)*SAFETY_CONST - (hrsElapsed * 0.015);
+  return ((gramsAlc / (bodyWeightGrams * genderConstant)) * 100);
 }
 
 export default calcBac;
