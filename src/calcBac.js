da2d03d5b9bc50eab0d0a4b866c8260767b2a617
Liam <liam.aharon@hotmail.com>
2017-03-05 19:40:20 +1100

    removed redundant variable

--- src/calcBac.js
+++ src/calcBac.js
@@ -2,7 +2,6 @@ const KG_TO_GRMS = 1000;
 const MS_TO_HR = 1/3600000;
 
 calcBac = (gramsAlc, bodyWeightKg, genderConstant) => {
-  curTime = new Date().getTime();
   bodyWeightGrams = bodyWeightKg * KG_TO_GRMS;
   return ((gramsAlc / (bodyWeightGrams * genderConstant)) * 100);
 }
