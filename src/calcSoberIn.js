742aa9973cc8cf6d68c5b9f665f555cf0907e497
Liam <liam.aharon@hotmail.com>
2017-03-05 19:42:29 +1100

    imporved accuracy

--- src/calcSoberIn.js
+++ src/calcSoberIn.js
@@ -2,7 +2,7 @@ const HR_TO_MS = 3600000;
 
 // limit is the bac we want to know the time that we'll be at
 calcSoberIn = (bac, limit) => {
-  return ((bac-limit)/0.015)*HR_TO_MS
+  return ((bac-limit)/0.016)*HR_TO_MS
 }
 
 export default calcSoberIn;
