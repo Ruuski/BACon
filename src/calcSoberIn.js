5efda2645b71319d18b925c10870ad1a24ca867d
Liam <liam.aharon@hotmail.com>
2017-03-05 19:47:30 +1100

    cleaned up

--- src/calcSoberIn.js
+++ src/calcSoberIn.js
@@ -1,8 +1,9 @@
 const HR_TO_MS = 3600000;
+const PERCENT_ALC_PROCESSED_PER_HR = 0.016;
 
 // limit is the bac we want to know the time that we'll be at
 calcSoberIn = (bac, limit) => {
-  return ((bac-limit)/0.016)*HR_TO_MS
+  return ((bac-limit)/PERCENT_ALC_PROCESSED_PER_HR)*HR_TO_MS
 }
 
 export default calcSoberIn;
