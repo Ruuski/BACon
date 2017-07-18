b31e30f2ec1af9085437f1c33a34c94f3a82f42c
liamaharon <liam.aharon@hotmail.com>
2017-07-18 12:27:57 +1000

    added basic profile settings screen

--- BACon/src/components/SettingsScreen.js
+++ BACon/src/components/SettingsScreen.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+import SettingsScreenStyles from './styles/SettingsScreenStyles';
+
 class SettingsScreen extends React.Component {
   render() {
-    const { bodyWeightKg, genderConstant }
+    // const { bodyWeightKg, genderConstant, updateProfile } = this.props;
+    return (
+
+      <View style={SettingsScreenStyles.container}>
+        <View style={SettingsScreenStyles.chooseSex}>
+          <Text>
+            Select your biological sex
+          </Text>
+        </View>
+        <View style={SettingsScreenStyles.chooseBodyWeight}>
+          <Text>
+            Enter your current body weight in kilograms
+          </Text>
+        </View>
+      </View>
+    )
   }
 }
+
+export default SettingsScreen;
---
+++ BACon/src/components/styles/SettingsScreenStyles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const SettingsScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default SettingsScreenStyles
+;
